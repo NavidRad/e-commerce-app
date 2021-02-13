dd0bd5e5c18991451a03292f9fc463a196e16486
NavidRad <navidrad.business@gmail.com>
2021-02-13 12:04:30 +0100

    adding sign out functionality and related links in the header

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ class App extends React.Component {
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
       this.setState({ currentUser: user });
-      console.log(user);
     });
   }
 
@@ -33,7 +32,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
